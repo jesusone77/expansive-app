c131611b519984d4c817755051eae36a5c03864e
“Jesusone77” <Jesus.e.b.h.one@gmail.com>
2025-06-16 17:45:34 -0600

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,27 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+    cssMinify: true,
+    minify: 'terser',
+    rollupOptions: {
+        output: {
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[1].toString();
+                }
+            },
+        },
+    },
+    terserOptions: {
+        compress: {
+            drop_console: true,
+            drop_debugger: true,
+        },
+        format: {
+            comments: false,
+        },
+    },
+  }
 })
